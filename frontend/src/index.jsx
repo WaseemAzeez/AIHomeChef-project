8135e7df5737972a990c6b0ae0566110272f4038
Waseem Aziz <waseemaziz026@gmail.com>
2025-08-10 08:33:39 +0500

    fix

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Main from './main.jsx'
+import Main from './Main.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
     <>
         <App />
+        <Main />
     </>
 )
