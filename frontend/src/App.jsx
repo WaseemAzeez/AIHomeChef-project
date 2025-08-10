46ded7dd4ce73c77d6ce6e88b81246a8e8368101
Waseem Aziz <waseemaziz026@gmail.com>
2025-08-10 08:36:04 +0500

    Fix the import issue in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Main from "./main";
+import Main from "./Main";
 export default function App() {
   return (
     <>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Main from './Main.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
     <>
         <App />
-        <Main />
     </>
 )
